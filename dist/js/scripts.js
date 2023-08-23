08bea8eaa0064faa88350e1bfe1e2c987520070e
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2023-08-23 17:50:53 +0200

    fix: add compiled files

---
+++ dist/js/scripts.js
@@ -0,0 +1,34 @@
+/*!
+* Start Bootstrap - Juan Carlos Farah's Resume v1.0.0 (https://juancarlosfarah.com)
+* Copyright 2013-2023 Juan Carlos Farah
+* Licensed under MIT (https://github.com/StartBootstrap/@juancarlosfarah/resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
